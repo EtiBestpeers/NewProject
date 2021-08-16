4d02cb1fb03724762aa940aa1edf6259b8aeab07
EtiBestpeers <etikhandelwal@bestpeers.com>
2021-08-16 22:09:28 +0530

    Practice

--- useeffect/src/App.js
+++ useeffect/src/App.js
@@ -4,16 +4,16 @@ import ClassCounterOne from './Components/ClassCounterOne';
 import ClassMouse from './Components/ClassMouse';
 import HookMouse from './Components/HookMouse';
 import HookCounterOne from './Components/HookCounterOne'
-
+import MouseContainer from './Components/MouseContainer';
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<ClassCounterOne /> 
-				 <HookCounterOne /> 
+				{/* <ClassCounterOne /> 
+				 <HookCounterOne />  */}
 				 {/* <ClassMouse></ClassMouse>  */}
-				 <HookMouse></HookMouse> 
-				
+				 {/* <HookMouse/> */}
+				<MouseContainer/>
 				 
 
 			</div>
