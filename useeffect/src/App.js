ac526656638fcde52aad933ee16b86cbd5860199
EtiBestpeers <etikhandelwal@bestpeers.com>
2021-08-16 21:56:21 +0530

    useEffect Hooks

--- useeffect/src/App.js
+++ useeffect/src/App.js
@@ -3,9 +3,6 @@ import './App.css'
 import ClassCounterOne from './Components/ClassCounterOne';
 import ClassMouse from './Components/ClassMouse';
 import HookMouse from './Components/HookMouse';
-import MouseContainer from './Components/MouseContainer';
-import IntervalClassCounter from './Components/IntervalClassCounter';
-import IntervalHookCounter from './Components/IntervalHookCounter';
 import HookCounterOne from './Components/HookCounterOne'
 
 class App extends Component {
@@ -16,8 +13,7 @@ class App extends Component {
 				 <HookCounterOne /> 
 				 {/* <ClassMouse></ClassMouse>  */}
 				 <HookMouse></HookMouse> 
-				 <MouseContainer /> 
-				 <IntervalClassCounter/>
+				
 				 
 
 			</div>
