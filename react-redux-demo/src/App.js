9d745ccf329343b36eaf4476ecb0bf62496fc7d8
EtiBestpeers <etikhandelwal@bestpeers.com>
2021-08-20 16:25:27 +0530

    Redux

--- react-redux-demo/src/App.js
+++ react-redux-demo/src/App.js
@@ -3,12 +3,15 @@ import { Provider } from 'react-redux';
 import './App.css';
 import store from './redux/store';
 import CakeContainer from './components/CakeContainer';
-
+import HooksCakeContainer from './components/HooksCakeContainer';
+import IceCreamContainer from './components/IceCreamContainer'
 function App() {
   return (
     <Provider store={store}>
     <div className="App">
+      <HooksCakeContainer/>
      <CakeContainer/>
+     <IceCreamContainer/>
     </div>
     </Provider> 
   );
--- react-redux-demo/src/redux/store.js
+++ react-redux-demo/src/redux/store.js
@@ -1,6 +1,9 @@
-import { createStore } from "redux";
-import cakeReducer from "./cake/cakeReducer";
+import { createStore, applyMiddleware } from "redux";
+import logger from 'redux-logger'
+import {composeWithDevTools} from 'redux-devtools-extension'
+import rootReducer from "./rootReducer"
 
-const store = createStore(cakeReducer)
 
-export default store
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)))
+
+export default store 
